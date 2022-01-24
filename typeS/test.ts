95ac09492a83167a0994ec087cee3f34e8d375fb
sododuk32 <sododuk63@gmail.com>
2022-01-24 19:17:11 +0900

    220124

--- typeS/test.ts
+++ typeS/test.ts
@@ -1,10 +1,12 @@
 function concat(left:string , right:string):string{
 
     return left+right;
-  
+    
   }
   
   
   let hello:string = concat("hello", "world");
   
   console.log(hello);
+
+  
