b98866a8a2c74fc8d47eb4c3f1dc0a87ca52f50e
sododuk32 <sododuk63@gmail.com>
2022-01-24 18:02:47 +0900

    reacting

---
+++ react-study/rayout/myapp/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Record from "./record";
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+  
+  <div className='body'>
+    <Record/>
+    <Record/>
+    <Record/>
+    <Record/>
+    <Record/>
+    <Record/>
+    <Record/>
+    
+
+
+    </div>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ typeS/test.ts
@@ -0,0 +1,10 @@
+function concat(left:string , right:string):string{
+
+    return left+right;
+  
+  }
+  
+  
+  let hello:string = concat("hello", "world");
+  
+  console.log(hello);
