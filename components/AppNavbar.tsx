ce9a9e18c589e3ea2e68b73a9a19e4a9538dcfaf
jh <97775986+jhp5234@users.noreply.github.com>
2022-02-17 18:06:52 +0900

    CoreUI 설치 + 내브바 생성

---
+++ components/AppNavbar.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { useState } from "react";
+import Head from "next/head";
+import {
+  CNavbar,
+  CContainer,
+  CNavbarBrand,
+  CNavbarToggler,
+  CCollapse,
+  CNavbarNav,
+  CNavItem,
+  CNavLink,
+  CDropdownToggle,
+  CDropdownMenu,
+  CDropdownItem,
+  CForm,
+  CDropdown,
+  CDropdownDivider,
+  CFormInput,
+  CButton,
+  CRow,
+  CCol,
+} from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import * as icon from "@coreui/icons";
+
+function AppNavbar() {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <>
+      <CNavbar expand="lg" colorScheme="light" className="bg-white">
+        <CContainer fluid>
+          <CNavbarBrand href="#">
+            <strong>NYMPHE</strong>
+          </CNavbarBrand>
+          <CNavbarToggler
+            aria-label="Toggle navigation"
+            aria-expanded={visible}
+            onClick={() => setVisible(!visible)}
+          />
+          <CCollapse className="navbar-collapse" visible={visible}>
+            <CNavbarNav className="me-auto">
+              <CNavItem>
+                <CNavLink href="#" active>
+                  아우터
+                </CNavLink>
+              </CNavItem>
+              <CNavItem>
+                <CNavLink href="#" active>
+                  상의
+                </CNavLink>
+              </CNavItem>
+              <CNavItem>
+                <CNavLink href="#" active>
+                  바지
+                </CNavLink>
+              </CNavItem>
+              <CNavItem>
+                <CNavLink href="#" active>
+                  스커트
+                </CNavLink>
+              </CNavItem>
+              <CNavItem>
+                <CNavLink href="#" active>
+                  원피스
+                </CNavLink>
+              </CNavItem>
+              <CNavItem>
+                <CNavLink href="#" active>
+                  신발
+                </CNavLink>
+              </CNavItem>
+              <CNavItem>
+                <CNavLink href="#" active>
+                  모자
+                </CNavLink>
+              </CNavItem>
+            </CNavbarNav>
+            <CNavbarNav>
+              <CForm className="d-flex">
+                <CFormInput type="search" placeholder="검색" />
+              </CForm>
+              <CIcon icon={icon.cilUser} size="xxl" className="ms-2" />
+              <CIcon icon={icon.cilBasket} size="xxl" className="ms-2" />
+            </CNavbarNav>
+          </CCollapse>
+        </CContainer>
+      </CNavbar>
+    </>
+  );
+}
+
+export default AppNavbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,42 @@
-import type { NextPage } from 'next';
-import styles from '../styles/Home.module.css';
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-    return (
-        <div className={styles.container}>
-            <h1>홈페이지</h1>
-        </div>
-    );
+  return (
+    <div className="bg-light">
+      <h1>홈페이지</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi nemo,
+        aperiam quidem earum et quo ex aliquid beatae praesentium asperiores id.
+        Incidunt provident, itaque numquam magnam dolore repudiandae cupiditate
+        odit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi nemo,
+        aperiam quidem earum et quo ex aliquid beatae praesentium asperiores id.
+        Incidunt provident, itaque numquam magnam dolore repudiandae cupiditate
+        odit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi nemo,
+        aperiam quidem earum et quo ex aliquid beatae praesentium asperiores id.
+        Incidunt provident, itaque numquam magnam dolore repudiandae cupiditate
+        odit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi nemo,
+        aperiam quidem earum et quo ex aliquid beatae praesentium asperiores id.
+        Incidunt provident, itaque numquam magnam dolore repudiandae cupiditate
+        odit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi nemo,
+        aperiam quidem earum et quo ex aliquid beatae praesentium asperiores id.
+        Incidunt provident, itaque numquam magnam dolore repudiandae cupiditate
+        odit.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
