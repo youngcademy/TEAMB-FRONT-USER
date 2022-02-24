26c91f3b14970e5a7f566ae2d8e9da6193620cb1
jh <97775986+jhp5234@users.noreply.github.com>
2022-02-24 15:42:04 +0900

    상품 리스트 페이지 추가 + 이미지 추가

---
+++ pages/items/list.tsx
@@ -0,0 +1,92 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+import leather from "../../public/leather.jpg";
+import slacks from "../../public/slacks.jpg";
+
+import Head from "next/head";
+import {
+  CNavbar,
+  CContainer,
+  CNavbarBrand,
+  CNavbarToggler,
+  CCollapse,
+  CNavbarNav,
+  CNavItem,
+  CNavLink,
+  CDropdownToggle,
+  CDropdownMenu,
+  CDropdownItem,
+  CForm,
+  CDropdown,
+  CDropdownDivider,
+  CFormInput,
+  CButton,
+  CRow,
+  CCol,
+  CCard,
+  CCardImage,
+  CCardBody,
+  CCardTitle,
+  CCardText,
+} from "@coreui/react";
+
+const List: NextPage = () => {
+  return (
+    <div className="bg-light">
+      <CRow xs={{ cols: 1 }} md={{ cols: 5 }} className="g-4">
+        <CCol xs>
+          <CCard className="h-100">
+            <Image src={leather} layout="responsive" width={400} height={400} />
+            <CCardBody>
+              <CCardTitle>핫한 여성용 가죽자켓</CCardTitle>
+              <CCardText>쎈언니</CCardText>
+              <CCardText>57,000원</CCardText>
+            </CCardBody>
+          </CCard>
+        </CCol>
+        <CCol xs>
+          <CCard className="h-100">
+            <Image src={slacks} layout="responsive" width={400} height={400} />
+            <CCardBody>
+              <CCardTitle>핫한 세미와이드 슬랙스</CCardTitle>
+              <CCardText>귀여운 마녀</CCardText>
+              <CCardText>22,000원</CCardText>
+            </CCardBody>
+          </CCard>
+        </CCol>
+        <CCol xs>
+          <CCard className="h-100">
+            <Image src={leather} layout="responsive" width={400} height={400} />
+            <CCardBody>
+              <CCardTitle>핫한 여성용 가죽자켓</CCardTitle>
+              <CCardText>쎈언니</CCardText>
+              <CCardText>57,000원</CCardText>
+            </CCardBody>
+          </CCard>
+        </CCol>
+        <CCol xs>
+          <CCard className="h-100">
+            <Image src={slacks} layout="responsive" width={400} height={400} />
+            <CCardBody>
+              <CCardTitle>핫한 세미와이드 슬랙스</CCardTitle>
+              <CCardText>귀여운 마녀</CCardText>
+              <CCardText>22,000원</CCardText>
+            </CCardBody>
+          </CCard>
+        </CCol>
+        <CCol xs>
+          <CCard className="h-100">
+            <Image src={leather} layout="responsive" width={400} height={400} />
+            <CCardBody>
+              <CCardTitle>핫한 여성용 가죽자켓</CCardTitle>
+              <CCardText>쎈언니</CCardText>
+              <CCardText>57,000원</CCardText>
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </div>
+  );
+};
+
+export default List;
