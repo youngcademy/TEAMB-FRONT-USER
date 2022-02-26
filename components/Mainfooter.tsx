dcf1d3b340b0f6f9902a12caa34a916286b53f50
sododuk32 <sododuk63@gmail.com>
2022-02-26 14:33:29 +0900

    프리티어 추가 및 기본 레이아웃 작성중

---
+++ components/Mainfooter.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Mainfooter: NextPage = () => {
+    return (
+        <>
+            <div>Mainfooter</div>
+        </>
+    );
+};
+
+export default Mainfooter;
---
+++ components/Mainheader.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import AppNavbar from "./AppNavbar";
+
+const Mainheader: NextPage = () => {
+  return (
+    <>
+      <AppNavbar />
+    </>
+  );
+};
+
+export default Mainheader;
