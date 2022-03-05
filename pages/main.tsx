74c38f8f9d65e251f35e69022016859d0f4a6b16
sododuk32 <sododuk63@gmail.com>
2022-03-05 14:17:14 +0900

    유저메인

--- pages/main.tsx
+++ pages/main.tsx
@@ -11,135 +11,141 @@ import {
     CCardFooter,
 } from '@coreui/react';
 
+// userfornt:main 전체 감쌈, userfrontimage : 가장큰 제품사진3개 , smallproducts:개별 상품사진 상자,
+// eventProduct: 특정주제에 맞는 상품 페이지 이동 사진,footer:푸터
 const Main: NextPage = () => {
     return (
         <>
             {/* 가장큰 제품사진 3개 */}
-            <div className="userfrontimagebox" style={styles1.container}>
+            <div className="userfront" style={styles1.container}>
                 <div className="userfrontimage" style={styles1.container}>
                     <Image
                         src="/img/image_one.jpg"
                         alt="1번사진"
-                        width={650}
+                        width={625}
                         height={800}
                     />
                     <Image
                         src="/img/image_two.jpg"
                         alt="2번사진"
-                        width={650}
+                        width={625}
                         height={800}
                     />
                     <Image
                         src="/img/image_three.jpg"
                         alt="3번사진"
-                        width={650}
+                        width={625}
                         height={800}
                     />
                 </div>
-                {/* 제품사진 */}
-                <CRow xs={{ cols: 1, gutter: 4 }} md={{ cols: 3 }}>
-                    <CCol xs>
-                        <CCard>
-                            <Image
-                                src="/img/image_one.jpg"
-                                alt="1번사진"
-                                width={250}
-                                height={400}
-                            />
-                            <CCardBody>
-                                <CCardTitle>product title1</CCardTitle>
-                                <CCardText>1</CCardText>
-                            </CCardBody>
-                        </CCard>
-                    </CCol>
-                    <CCol xs>
-                        <CCard>
-                            <Image
-                                src="/img/image_one.jpg"
-                                alt="1번사진"
-                                width={250}
-                                height={400}
-                            />
-                            <CCardBody>
-                                <CCardTitle>product title2</CCardTitle>
-                                <CCardText>2</CCardText>
-                            </CCardBody>
-                        </CCard>
-                    </CCol>
-                    <CCol xs>
-                        <CCard>
-                            <Image
-                                src="/img/image_one.jpg"
-                                alt="1번사진"
-                                width={250}
-                                height={400}
-                            />
-                            <CCardBody>
-                                <CCardTitle>product title3</CCardTitle>
-                                <CCardText>3</CCardText>
-                            </CCardBody>
-                        </CCard>
-                    </CCol>
-                    <CCol xs>
-                        <CCard>
-                            <Image
-                                src="/img/image_one.jpg"
-                                alt="1번사진"
-                                width={150}
-                                height={300}
-                            />
-                            <CCardBody>
-                                <CCardTitle>product title4</CCardTitle>
-                                <CCardText>4</CCardText>
-                            </CCardBody>
-                        </CCard>
-                    </CCol>
+                {/* 제품사진 md={cols:4}는 가로로 카드4개넘으면 wrap */}
+                {/* gutter:카드 간 간격 */}
+                <div className="smallproducts">
+                    <CRow xs={{ cols: 1, gutter: 4 }} md={{ cols: 3 }}>
+                        <CCol xs>
+                            <CCard>
+                                <Image
+                                    src="/img/image_one.jpg"
+                                    alt="1번사진"
+                                    width={250}
+                                    height={400}
+                                />
+                                <CCardBody>
+                                    <CCardTitle>product title1</CCardTitle>
+                                    <CCardText>1</CCardText>
+                                </CCardBody>
+                            </CCard>
+                        </CCol>
+                        <CCol xs>
+                            <CCard>
+                                <Image
+                                    src="/img/image_one.jpg"
+                                    alt="1번사진"
+                                    width={100}
+                                    height={400}
+                                />
+                                <CCardBody>
+                                    <CCardTitle>product title2</CCardTitle>
+                                    <CCardText>2</CCardText>
+                                </CCardBody>
+                            </CCard>
+                        </CCol>
+                        <CCol xs>
+                            <CCard>
+                                <Image
+                                    src="/img/image_one.jpg"
+                                    alt="1번사진"
+                                    width={250}
+                                    height={400}
+                                />
+                                <CCardBody>
+                                    <CCardTitle>product title3</CCardTitle>
+                                    <CCardText>3</CCardText>
+                                </CCardBody>
+                            </CCard>
+                        </CCol>
+                        <CCol xs>
+                            <CCard>
+                                <Image
+                                    src="/img/image_one.jpg"
+                                    alt="1번사진"
+                                    width={150}
+                                    height={300}
+                                />
+                                <CCardBody>
+                                    <CCardTitle>product title4</CCardTitle>
+                                    <CCardText>4</CCardText>
+                                </CCardBody>
+                            </CCard>
+                        </CCol>
 
-                    <CCol xs>
-                        <CCard>
-                            <Image
-                                src="/img/image_one.jpg"
-                                alt="1번사진"
-                                width={150}
-                                height={300}
-                            />
-                            <CCardBody>
-                                <CCardTitle>product title4</CCardTitle>
-                                <CCardText>4</CCardText>
-                            </CCardBody>
-                        </CCard>
-                    </CCol>
+                        <CCol xs>
+                            <CCard>
+                                <Image
+                                    src="/img/image_one.jpg"
+                                    alt="1번사진"
+                                    width={150}
+                                    height={300}
+                                />
+                                <CCardBody>
+                                    <CCardTitle>product title5</CCardTitle>
+                                    <CCardText>4</CCardText>
+                                </CCardBody>
+                            </CCard>
+                        </CCol>
 
-                    <CCol xs>
-                        <CCard>
-                            <Image
-                                src="/img/image_one.jpg"
-                                alt="1번사진"
-                                width={150}
-                                height={300}
-                            />
-                            <CCardBody>
-                                <CCardTitle>product title4</CCardTitle>
-                                <CCardText>4</CCardText>
-                            </CCardBody>
-                        </CCard>
-                    </CCol>
+                        <CCol xs>
+                            <CCard>
+                                <Image
+                                    src="/img/image_one.jpg"
+                                    alt="1번사진"
+                                    width={150}
+                                    height={300}
+                                />
+                                <CCardBody>
+                                    <CCardTitle>product title6</CCardTitle>
+                                    <CCardText>4</CCardText>
+                                </CCardBody>
+                            </CCard>
+                        </CCol>
 
-                    <CCol xs>
-                        <CCard>
-                            <Image
-                                src="/img/image_one.jpg"
-                                alt="1번사진"
-                                width={150}
-                                height={300}
-                            />
-                            <CCardBody>
-                                <CCardTitle>product title4</CCardTitle>
-                                <CCardText>4</CCardText>
-                            </CCardBody>
-                        </CCard>
-                    </CCol>
-                </CRow>
+                        <CCol xs>
+                            <CCard>
+                                <Image
+                                    src="/img/image_one.jpg"
+                                    alt="1번사진"
+                                    width={150}
+                                    height={300}
+                                />
+                                <CCardBody>
+                                    <CCardTitle>product title7</CCardTitle>
+                                    <CCardText>4</CCardText>
+                                </CCardBody>
+                            </CCard>
+                        </CCol>
+                    </CRow>
+                </div>
+
                 {/* 이벤트 사진 2개 */}
                 <div className="eventProduct">
                     <Image
@@ -159,18 +165,25 @@ const Main: NextPage = () => {
                 <div className="footer">
                     <div className="footerlogo">
                         <a>
-                            {/* <Image
-                            src="./img/nodejslogo.jpg"
-                            width={130}
-                            height={17}
-                        /> */}
+                            <Image
+                                src="/img/nodejslogo.jpg"
+                                width={130}
+                                height={17}
+                            />
                             <span>사업자명 : (주)영카데미b</span>
                             <span>전화 : 010-8468-4897</span>
-                            <span>가</span>
-                            <span>나</span>
-                            <span>다</span>
-                            <span>라</span>
-                            <span>마</span>
+                            <div className="footerpart2">
+                                <span>가</span>
+                                <span>나</span>
+                                <span>다</span>
+                                <br></br>
+                                <span>라</span>
+                                <span>마</span>
+                                <span>바</span>
+                                <br></br>
+                                <span>사</span>
+                                <span>아</span>
+                            </div>
                         </a>
                     </div>
                 </div>
