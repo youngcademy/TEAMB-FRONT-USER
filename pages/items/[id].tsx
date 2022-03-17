cdcad43efc1f4e422bfebf9b1a1624a1c06fee77
jh <97775986+jhp5234@users.noreply.github.com>
2022-03-17 15:49:16 +0900

    Update [id].tsx

--- pages/items/[id].tsx
+++ pages/items/[id].tsx
@@ -29,34 +29,34 @@ import {
 import type { NextPage } from 'next';
 import Image from 'next/image';
 
-const ID: NextPage = () => {
+const Item: NextPage = () => {
     return (
         <>
             <CCard className="mb-1">
-                <CCardHeader>상품 수정</CCardHeader>
+                <CCardHeader component="h5">상품 수정</CCardHeader>
                 <CCardBody>
-                    <CRow
-                        xs={{ cols: 1, gutter: 4 }}
-                        md={{ cols: 2 }}
-                        className="row justify-content-md-center"
-                    >
-                        <CCol md={4}>
-                            <Image
-                                src="/pants.jpg"
-                                layout="responsive"
-                                width={300}
-                                height={300}
-                                alt=""
-                            />
+                    <CRow sm={{ cols: 1, gutter: 3 }} md={{ cols: 2 }}>
+                        <CCol>
+                            <CRow className="justify-content-center">
+                                <Image
+                                    src="/pants.jpg"
+                                    layout="fixed"
+                                    width={500}
+                                    height={700}
+                                    alt=""
+                                />
+                            </CRow>
                         </CCol>
-                        <CCol md={5}>
+                        <CCol>
                             <CCard>
                                 <CCardBody>
                                     <CForm>
                                         <CRow className="mb-3">
-                                            <CFormLabel className="col-sm-2 col-form-label">
-                                                이미지 선택
-                                            </CFormLabel>
+                                            <CCol xs={2}>
+                                                <CFormLabel className="col-form-label">
+                                                    이미지 선택
+                                                </CFormLabel>
+                                            </CCol>
                                             <CCol>
                                                 <CFormInput
                                                     type="file"
@@ -65,25 +65,31 @@ const ID: NextPage = () => {
                                             </CCol>
                                         </CRow>
                                         <CRow className="mb-3">
-                                            <CFormLabel className="col-sm-2 col-form-label">
-                                                상품명
-                                            </CFormLabel>
+                                            <CCol xs={2}>
+                                                <CFormLabel className="col-form-label">
+                                                    상품명
+                                                </CFormLabel>
+                                            </CCol>
                                             <CCol>
                                                 <CFormInput type="text" />
                                             </CCol>
                                         </CRow>
                                         <CRow className="mb-3">
-                                            <CFormLabel className="col-sm-2 col-form-label">
-                                                브랜드
-                                            </CFormLabel>
+                                            <CCol xs={2}>
+                                                <CFormLabel className="col-form-label">
+                                                    브랜드
+                                                </CFormLabel>
+                                            </CCol>
                                             <CCol>
                                                 <CFormInput type="text" />
                                             </CCol>
                                         </CRow>
                                         <CRow className="mb-3">
-                                            <CFormLabel className="col-sm-2 col-form-label">
-                                                상품 분류
-                                            </CFormLabel>
+                                            <CCol xs={2}>
+                                                <CFormLabel className="col-form-label">
+                                                    상품 분류
+                                                </CFormLabel>
+                                            </CCol>
                                             <CCol>
                                                 <CFormSelect aria-label="Default select example">
                                                     <option>선택하세요</option>
@@ -100,10 +106,12 @@ const ID: NextPage = () => {
                                             </CCol>
                                         </CRow>
                                         <CRow className="mb-3">
-                                            <CFormLabel className="col-sm-2 col-form-label">
-                                                컬러
-                                            </CFormLabel>
-                                            <CCol>
+                                            <CCol xs={2}>
+                                                <CFormLabel className="col-form-label">
+                                                    컬러
+                                                </CFormLabel>
+                                            </CCol>
+                                            <CCol xs={5}>
                                                 <CFormSelect aria-label="Default select example">
                                                     <option>컬러 목록</option>
                                                     <option value="1">
@@ -127,10 +135,12 @@ const ID: NextPage = () => {
                                             </CCol>
                                         </CRow>
                                         <CRow className="mb-3">
-                                            <CFormLabel className="col-sm-2 col-form-label">
-                                                사이즈
-                                            </CFormLabel>
-                                            <CCol>
+                                            <CCol xs={2}>
+                                                <CFormLabel className="col-form-label">
+                                                    사이즈
+                                                </CFormLabel>
+                                            </CCol>
+                                            <CCol xs={5}>
                                                 <CFormSelect aria-label="Default select example">
                                                     <option>사이즈 목록</option>
                                                     <option value="1">
@@ -153,27 +163,17 @@ const ID: NextPage = () => {
                                                 </CButton>
                                             </CCol>
                                         </CRow>
-                                        <CRow className="mb-5">
-                                            <CFormLabel className="col-sm-2 col-form-label">
-                                                판매 가격
-                                            </CFormLabel>
+                                        <CRow className="mb-3">
+                                            <CCol xs={2}>
+                                                <CFormLabel className="col-form-label">
+                                                    판매 가격
+                                                </CFormLabel>
+                                            </CCol>
                                             <CCol>
                                                 <CFormInput type="text" />
                                             </CCol>
                                         </CRow>
                                     </CForm>
-                                    <div className="d-grid gap-2 d-md-flex justify-content-md-center">
-                                        <CButton
-                                            size="lg"
-                                            color="primary"
-                                            className="me-md-3"
-                                        >
-                                            구입하기
-                                        </CButton>
-                                        <CButton color="secondary" size="lg">
-                                            장바구니
-                                        </CButton>
-                                    </div>
                                 </CCardBody>
                             </CCard>
                         </CCol>
@@ -182,28 +182,19 @@ const ID: NextPage = () => {
             </CCard>
             <CCard className="mb-1">
                 <CCardBody>
-                    <CRow>
-                        <CForm>
-                            <div className="mb-3">
-                                <CFormLabel htmlFor="exampleFormControlTextarea1">
-                                    상품 상세설명
-                                </CFormLabel>
-                                <CFormTextarea
-                                    id="exampleFormControlTextarea1"
-                                    rows="10"
-                                ></CFormTextarea>
-                            </div>
-                        </CForm>
-                    </CRow>
-                    <CRow>
+                    <CForm>
+                        <div className="mb-3">
+                            <CFormLabel>상품 상세설명</CFormLabel>
+                            <CFormTextarea rows="10"></CFormTextarea>
+                        </div>
                         <div className="d-grid col-1 mx-auto">
                             <CButton color="primary">등록</CButton>
                         </div>
-                    </CRow>
+                    </CForm>
                 </CCardBody>
             </CCard>
         </>
     );
 };
 
-export default ID;
+export default Item;
