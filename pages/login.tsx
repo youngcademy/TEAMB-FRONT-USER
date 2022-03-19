df09fd5e387e8c11f41344a2535ce5e35dd0863b
gkssk1026 <gkssk1026@hanmail.net>
2022-03-19 20:56:27 +0900

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -9,27 +9,83 @@ import {
     CCardTitle,
     CCardText,
     CCardFooter,
+    CForm,
+    CFormLabel,
+    CFormInput,
+    CFormText,
+    CButton,
+    CFormCheck,
+    CContainer,
 } from '@coreui/react';
 
 const Main: NextPage = () => {
     return (
         <>
+            <CCardBody>
+                <CCardTitle>Login</CCardTitle>
+            </CCardBody>
+            <CCard>
+                <img
+                    src="https://images.unsplash.com/photo-1560849735-881abdac6a3d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=dan-smedley-RqTFUay48so-unsplash.jpg"
+                    alt="login"
+                    width={500}
+                    height={400}
+                />
+            </CCard>
+            <CContainer>
+                <CRow>
+                    <CCol className="align-self-center"></CCol>
+                    <CForm>
+                        <div className="mb-3">
+                            <div className="container w-50">
+                                <CFormLabel htmlFor="exampleInputEmail1">
+                                    Email address
+                                </CFormLabel>
+                                <CFormInput
+                                    type="email"
+                                    id="exampleInputEmail1"
+                                    aria-describedby="emailHelp"
+                                />
+                                <CFormText id="emailHelp">
+                                    We'll never share your email with anyone
+                                    else.
+                                </CFormText>
+                            </div>
+                        </div>
+                        <div className="mb-3">
+                            <div className="container w-50">
+                                <CFormLabel htmlFor="exampleInputPassword1">
+                                    Email Password
+                                </CFormLabel>
+                                <CFormInput
+                                    type="password"
+                                    id="exampleInputPassword1"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="container w-50">
+                            <CButton type="submit" color="primary">
+                                Submit
+                            </CButton>
+                        </div>
+                    </CForm>
+                </CRow>
+            </CContainer>
+
             {/* footer */}
             <div className="footer">
                 <div className="footerlogo">
                     <a>
                         {/* <Image
-                            src="./img/nodejslogo.jpg"
+                            src="https://unsplash.com/photos/RqTFUay48so"
                             width={130}
                             height={17}
                         /> */}
-                        <span>사업자명 : (주)영카데미b</span>
-                        <span>전화 : 010-8468-4897</span>
-                        <span>가</span>
-                        <span>나</span>
-                        <span>다</span>
-                        <span>라</span>
-                        <span>마</span>
+                        <li>
+                            <span>대표자명 : TeamB</span>
+                        </li>
+                        <span>대표전화 : 010-1234-5678</span>
                     </a>
                 </div>
             </div>
@@ -38,7 +94,7 @@ const Main: NextPage = () => {
 };
 
 const styles1 = {
-    container: { padding: '0.5rem' },
+    container: { padding: '0.5em' },
 } as const;
 
 export default Main;
